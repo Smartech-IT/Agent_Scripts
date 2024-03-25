8c98bc6ac2b300a0898f06ce34c865a4b85d03da
Smartech-IT.eu <147044456+Smartech-IT@users.noreply.github.com>
2024-03-25 12:43:37 +0100

    Update Install Agent.ps1

--- Windows/Install Agent.ps1
+++ Windows/Install Agent.ps1
@@ -34,7 +34,7 @@ while ($Insert_Test) {
 Write-Host $line
 $content = Get-Content -Path "C:\Program Files (x86)\ossec-agent\ossec.conf"
 
-$content[10] = "      <address>178.218.246.60</address>"
+$content[10] = "      <address>83.168.108.76</address>"
 $content[11] = "      <port>$Client_port</port>"
 $content[18] = "      <enrollment>`n        <manager_address>178.218.246.60</manager_address>`n        <port>$Enrollment_port</port>`n        <agent_name>$Agent_name</agent_name>`n        <groups>$Wazuh_Agent_Group</groups>`n      </enrollment>`n  </client>"
 
